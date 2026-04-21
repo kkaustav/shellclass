d1d60e7088437f34ab05aa442e4c640be349f882
EC2 Default User <ec2-user@ip-172-31-64-4.ec2.internal>
2026-04-21 12:16:08 +0000

    auto-save: hello.sh 2026-04-21 12:16

--- hello.sh
+++ hello.sh
@@ -32,3 +32,13 @@ echo "you are going to write lots of ${WORD}${ENDING} in this class!"
 
 # test auto push
 echo "you are going to write lots of ${WORD}${ENDING} in this class!"
+
+echo ""${WORD}${ENDING} is fun!""
+
+echo "This is a shell ${WORD}"
+
+echo "this is ${WORD}${ENDING}"
+
+echo '$WORD'
+
+echo "This is a shell ${WORD}"
